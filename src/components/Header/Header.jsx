58214727cbeb634c33504d00af7246cf3f345cc4
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-07-23 18:19:09 +0530

    designed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,10 @@ import './Header.css'
 const Header = () => {
     return(
         <div className="header">
-            <div className="header-left">
-                <h2>Cake</h2>
-                <div>
-                    <p></p>
-                </div>
-                <div className="header-btn">
-                    <div>Order Now</div>
-                </div>
+            <div className="header-contents">
+                <h2>Order Your Favorite Cake</h2>
+                <p>Welcome to Cake Shop! Irresistible cakes, freshly baked to delight every celebration and craving</p>
+                <button>Order Now</button>
             </div>
         </div>
     )
