b4001e42510c9736e7db046c949874e0c971e79d
Thasuni Induma <138390612+ThasuniInduma@users.noreply.github.com>
2025-07-23 18:50:19 +0530

    header image update

--- src/components/assets/assets.js
+++ src/components/assets/assets.js
@@ -1,7 +1,10 @@
 import search from'./search.png'
 import bascket from './bascket.png'
+import header from './header-img.png'
+
 
 export const assets = {
     search,
-    bascket
+    bascket,
+    header
 }
